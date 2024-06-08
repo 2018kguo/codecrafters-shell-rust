932a7bde0a46c9002ca321e0bd66b87f5b36d981
Kevin Guo <kev.guo123@gmail.com>
2024-06-07 23:28:34 -0400

    repl

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,25 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     //println!("Logs from your program will appear here!");
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-
-    match input.trim() {
-        "cd" => {
-            println!("cd: missing argument");
-        }
-        "echo" => {
-            println!("echo: missing argument");
-        }
-        _ => {
-            println!("{}: command not found", input.trim());
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        
+        match input.trim() {
+            "cd" => {
+                println!("cd: missing argument");
+            }
+            "echo" => {
+                println!("echo: missing argument");
+            }
+            _ => {
+                println!("{}: command not found", input.trim());
+            }
         }
     }
 }
